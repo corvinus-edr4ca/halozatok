0c3b3b52e8fabd00729fb59ab48358f10c30d127
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-04-17 12:41:44 +0200

    feladatsor kérdések kontrollerrel

---
+++ Hajosteszt/Hajosteszt/Controllers/TesztController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hajosteszt.Controllers
+{
+    
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            string pontosIdo = DateTime.Now.ToShortTimeString();
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = szoveg.ToUpper()
+            };
+        }
+    }
+}
---
+++ Hajosteszt/Hajosteszt/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hajosteszt.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public byte CorrectAnswer { get; set; }
+        public string Image { get; set; }
+    }
+}
