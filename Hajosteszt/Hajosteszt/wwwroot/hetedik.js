b8fe6a331d1932086a4b51de7336f6c29f070d8d
András Pataky <andras.pataky@stud.uni-corvinus.hu>
2021-04-25 17:28:38 +0200

    kerdesbetoltesek api controller-rel

--- Hajosteszt/Hajosteszt/wwwroot/hetedik.js
+++ Hajosteszt/Hajosteszt/wwwroot/hetedik.js
@@ -1,43 +1,49 @@
 ﻿var kérdések;
-var kérdésszám = 0;
-var összkérdés;
+var kérdésszám = 1;
+var összkérdés = 859;
 var kattintott;
+var helyes;
 
-function letöltés() {
-    fetch('/questions.json')
-        .then(r => r.json())
-        .then(data => letöltésBefejeződött(data));
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            } else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
 }
 
-function letöltésBefejeződött(d) {
-    console.log("Sikeres letöltés");
-    console.log(d);
-    kérdések = d;
-    összkérdés = kérdések.length;
-    console.log(összkérdés);
-    kérdésMegjelenítés(kérdések);
-}
 
 function kérdésMegjelenítés(kérdés) {
-    
-    let kerdesszoveg = document.getElementById("kerdesszoveg");
-    let valasz1 = document.getElementById("valasz1");
-    let valasz2 = document.getElementById("valasz2");
-    let valasz3 = document.getElementById("valasz3");
+
+    document.getElementById("kerdesszoveg").innerText = kérdés.questionText;
+    document.getElementById("valasz1").innerText = kérdés.answer1;
+    document.getElementById("valasz2").innerText = kérdés.answer2;
+    document.getElementById("valasz3").innerText = kérdés.answer3;
     let kep1 = document.getElementById("kep1");
-    kerdesszoveg.innerHTML = (kérdésszám+1) + ". " + kérdések[kérdésszám].questionText;
-    valasz1.innerHTML = kérdések[kérdésszám].answer1;
-    valasz2.innerHTML = kérdések[kérdésszám].answer2;
-    valasz3.innerHTML = kérdések[kérdésszám].answer3;
-    kep1.src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdésszám].image;
+   
+    if (!kérdés.image == "") {
+        kep1.src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+        kep1.style.display = 'flex';
+    } else {
+        kep1.style.display = 'none';
+    }
+
+    helyes = kérdés.correctAnswer;
 
     valasz1.classList.remove("jo", "rossz");
     valasz2.classList.remove("jo", "rossz");
     valasz3.classList.remove("jo", "rossz");
 }
 
+
 window.onload = function () {
-    letöltés();
+    kérdésBetöltés(kérdésszám);
+
+
     document.getElementById("valasz1").onclick = function () {
         kattintott = 1;
         ellenőrzés();
@@ -53,35 +59,34 @@ window.onload = function () {
 }
 
 function lépTovább() {
-    if (kérdésszám < összkérdés - 1) {
+    if (kérdésszám < összkérdés) {
         kérdésszám++;
-        kérdésMegjelenítés();
+        kérdésBetöltés(kérdésszám);
     }
     else {
-        kérdésszám = 0;
-        kérdésMegjelenítés();
+        kérdésszám = 1;
+        kérdésBetöltés(kérdésszám);
     }
 }
 
 function lépVissza() {
-    if (kérdésszám > 0) {
+    if (kérdésszám > 1) {
         kérdésszám--;
-        kérdésMegjelenítés();
+        kérdésBetöltés(kérdésszám);
     }
     else {
-        kérdésszám = összkérdés -1 ;
-        kérdésMegjelenítés();
+        kérdésszám = összkérdés;
+        kérdésBetöltés(kérdésszám);
     }
 }
 
 
 function ellenőrzés() {
-    let jovalasz = kérdések[kérdésszám].correctAnswer;
-    if (kattintott == jovalasz) {
+    if (kattintott == helyes) {
         document.getElementById("valasz" + kattintott).classList.add("jo")
     }
     else {
         document.getElementById("valasz" + kattintott).classList.add("rossz")
-        document.getElementById("valasz" + jovalasz).classList.add("jo")
+        document.getElementById("valasz" + helyes).classList.add("jo")
     }
 }
